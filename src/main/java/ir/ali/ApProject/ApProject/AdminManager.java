3e56138b5630cc4c096aa9071c2d715ee376b16c
maryam.sadeghi <miryamsadeghi82@gmail.com>
2021-07-09 13:01:06 +0430

    admin manager best seller and login counter

---
+++ src/main/java/ir/ali/ApProject/ApProject/AdminManager.java
@@ -0,0 +1,38 @@
+package ir.ali.ApProject.ApProject;
+
+
+import java.util.*;
+import java.util.Map.Entry;
+import java.util.stream.Collectors;
+
+public class AdminManager {
+
+    public HashMap<String,Integer>  findBestSeller(ArrayList<User>  allUsers,ArrayList<Product> allProducts ){
+
+        HashMap<String,Integer> unSortedSellers=new HashMap<>();
+
+        for(Product product:allProducts){   ///create unsortedHashmap
+            if(product.buyerID!=null)
+            {
+                for(User user:allUsers){
+                    if(product.sellerID.equals(user.email)){
+                        Integer count = unSortedSellers.containsKey(product.sellerID) ? unSortedSellers.get(product.sellerID) : 0;
+                        unSortedSellers.put(product.sellerID, count+1);
+
+                    }
+                }
+            }
+        }
+
+        HashMap<String, Integer> sortedSellers    ///sort the hashmap by value
+                = unSortedSellers.entrySet()
+                .stream()
+                .sorted(Collections.reverseOrder(Entry.comparingByValue()))
+                .collect(Collectors.toMap(Entry::getKey, Entry::getValue, (e1, e2) -> e1, LinkedHashMap::new));
+
+        return sortedSellers;
+
+    }
+
+
+}
